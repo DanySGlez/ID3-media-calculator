e5bde8689021dce6cf4c864db013060c78b455d7
Daniel Salomón <66494746+DanySGlez@users.noreply.github.com>
2023-05-20 22:36:13 -0300

    initial commit

---
+++ scripts.js
@@ -0,0 +1,18 @@
+var nome = "Dani"
+
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 7
+var notaDoTerceiroBimestre = 5
+var notaDoQuartoBimestre = 3
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem vindo " + nome)
+console.log(notaFixada)
+
+// esto es un comentario 
+
+// Revisão
+// Variáveis, strings, console.log, toFixed, operações matemáticas, concatenação
